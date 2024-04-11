5c462ccd053b083d038717862e9aa954dc2884a9
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-04-11 11:50:03 +0200

    pico done

--- picocss/README.md
+++ picocss/README.md
@@ -6,6 +6,23 @@
   </a>
 </p>
 
+## Installation
+
+Pour installer PicoCSS, il faut aller sur la documentation officiel de [PicoCSS](https://picocss.com/docs).
+
+### Steps
+
+1. `npm install @picocss/pico`
+2. `npm i -D sass`
+3. changer le ficher `index.css` vers `index.scss` et de changer le nom de l'import dans `main.jsx`
+4. Ajouter le code suivant dans le fichier `index.scss`
+
+```scss
+@use "@picocss/pico/scss/pico";
+```
+
+5. Et voilà ! Maintenant, vous avez la possibilité d'utiliser PicoCSS dans votre projet et d'utiliser votre propre css.
+
 ## Usage
 
 ```sh
--- picocss/src/App.jsx
+++ picocss/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<div className="container">
+			<h1 className="text-center">Hello, PicoCSS!</h1>
+			<article>
+				<header>Je souhaite m'inscrire</header>
+				<form>
+					<fieldset>
+						<label>
+							First name
+							<input
+								name="first_name"
+								placeholder="First name"
+								autocomplete="given-name"
+							/>
+						</label>
+						<label>
+							Email
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								autocomplete="email"
+							/>
+						</label>
+					</fieldset>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+					<input type="submit" value="Subscribe" />
+				</form>
+				<footer>Je suis le footer</footer>
+			</article>
+			<h2>Les loaders</h2>
+			<section className="grid">
+				<button aria-busy="true" aria-label="Please wait…" />
+				<button
+					aria-busy="true"
+					aria-label="Please wait…"
+					class="secondary"
+				/>
+				<button
+					aria-busy="true"
+					aria-label="Please wait…"
+					class="contrast"
+				/>
+				<button aria-busy="true" class="outline">
+					Please wait…
+				</button>
+				<button aria-busy="true" class="outline secondary">
+					Please wait…
+				</button>
+				<button aria-busy="true" class="outline contrast">
+					Please wait…
+				</button>
+			</section>
+		</div>
+	);
 }
 
-export default App
+export default App;
